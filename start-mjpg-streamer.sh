3d4866c1d6f7b406f596bc63d709325cd46e4a3a
WangLei <nihiue@gmail.com>
2022-10-28 22:50:45 +0800

    fix typo

--- start-mjpg-streamer.sh
+++ start-mjpg-streamer.sh
@@ -1,1 +1,1 @@
-mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 1920x1080 -f 30' -o 'output_http.so -p 8010
+mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 1920x1080 -f 30' -o 'output_http.so -p 8010'
